379c21df03a78af6a8551b033fe7452360378197
YuanDang <viviandang121@gmail.com>
2022-12-09 15:59:08 -0500

    update dataclean

--- Dataset/FinalProj_dataclean.py
+++ Dataset/FinalProj_dataclean.py
@@ -4,11 +4,14 @@ import pandas as pd
 import seaborn as sns
 import re
 import nltk
+import os
+from unidecode import unidecode
 
 
 # ------------------------preprocessing------------------------
 # ----------load data
-dataset = pd.read_csv('train.csv')
+# dataset = pd.read_csv('train.csv')
+dataset = pd.read_csv('train.csv.zip', encoding='utf-8')
 data = dataset[['discourse_text', 'discourse_type']].copy()
 # ----------check null values
 print('missing values exists:\n', data.isnull().any())
@@ -23,7 +26,10 @@ print(data.head())
 
 
 # ----------clean text
+print(f'Text contains non-ASCII characters, for example: {data.discourse_text.iloc[144290].encode()}')
 def clean_text(x):
+    # ----------convert text to ASCII form
+    x = unidecode(x)
     # ----------lowercase
     x = x.lower()
     # ----------remove url
@@ -33,6 +39,7 @@ def clean_text(x):
 
 data['text'] = data['discourse_text'].astype(str).apply(lambda x: clean_text(x))
 
+print(f'after cleaning: {data.text.iloc[144290]}')
 # ----------remove stop words
 stop_words = nltk.corpus.stopwords.words('english')
 
@@ -40,7 +47,7 @@ def remove_stop_words(corpus):
     result = []
     corp = corpus.split(' ')
     result = [w for w in corp if w not in stop_words]
-    result = " ".join(result)
+    result = " ".join(result).strip()
 
     return result
 
@@ -50,18 +57,24 @@ data['text'] = data['text'].apply(lambda x: remove_stop_words(x))
 lemma = nltk.WordNetLemmatizer()
 data['text'] = data.text.apply(lambda x: lemma.lemmatize(x))
 
+# ----------remove non-sense text
+print('Dataset has text with no sense:')
+print(data[data.text==""])
+data = data[data.text!=""].reset_index()
+
 # ----------tokenize
-from transformers import AutoTokenizer
-checkpoint = "distilbert-base-uncased"
-tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-data['text'] = data['text'].apply(lambda x:tokenizer.tokenize(x, truncation=True, return_tensors="pt"))
-print(data.head())
+# from transformers import AutoTokenizer
+# checkpoint = "distilbert-base-uncased"
+# tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+# data['text'] = data['text'].apply(lambda x:tokenizer.tokenize(x, truncation=True, return_tensors="pt"))
+# print(data.head())
 
 # data['sent_token'] = data['text'].apply(lambda x: sent_tokenize(x))
 # print(data.sent_token.head())
 
 # ----------savedata
-df = data.filter(['text', 'discourse_text', 'label'])
-df.to_csv('clean_train.csv')
+df = data.filter(['text', 'discourse_text', 'discourse_type', 'label'])
+path = os.getcwd()
+df.to_csv(f'{path}/clean_train.csv')
 
 
